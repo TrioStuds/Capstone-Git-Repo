a8affed8f5a10d60e155e6a5b1acc6d28c265163
Deion <triostuds@gmail.com>
2025-09-17 19:21:54 -0700

    updated login page, customer home, base customer and app.py for routing

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/customer_home')
+def customer_home():
+    return render_template('customer_home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
